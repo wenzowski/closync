5f12b6336347284c08cc3b3fba15c1c5190eeba5
Alexander Wenzowski <alexander@wenzowski.com>
2012-10-23 20:12:32 -0400

    delete missing files from remote and only upload stale files

--- spec/lib/closync/sync_spec.rb
+++ spec/lib/closync/sync_spec.rb
@@ -15,26 +15,60 @@ describe Closync::Storage do
     config
   }
 
-  context 'local sync' do
-    before(:each) do
-      FileUtils.mkdir_p local_dir
-      FileUtils.mkdir_p remote_dir
-      FileUtils.touch "#{local_dir}/index.html"
-      FileUtils.touch "#{local_dir}/pic.jpg"
-    end
-    after(:each) do
-      FileUtils.rm_rf local_dir
-      FileUtils.rm_rf remote_dir
-    end
+  before(:each) do
+    FileUtils.mkdir_p local_dir
+    FileUtils.mkdir_p remote_dir
+    FileUtils.touch "#{remote_dir}/missing.html"
+    FileUtils.touch "#{remote_dir}/stale.html"
+    FileUtils.touch "#{local_dir}/index.html"
+    FileUtils.touch "#{local_dir}/pic.jpg"
+    File.open("#{local_dir}/stale.html", 'w') {|f| f.write('new data') }
+  end
+  after(:each) do
+    FileUtils.rm_rf local_dir
+    FileUtils.rm_rf remote_dir
+  end
 
-    it { lambda {Closync::Sync.new(config)}.should_not raise_error }
+  it { lambda {Closync::Sync.new(config)}.should_not raise_error }
 
-    it 'pushes files to remote' do
+  context 'before sync' do
+    its 'new files exist locally' do
+      File.exists?("#{local_dir}/index.html").should be_true
+      File.exists?("#{local_dir}/pic.jpg").should be_true
+    end
+    its 'new files do not exist on remote' do
       File.exists?("#{remote_dir}/index.html").should be_false
       File.exists?("#{remote_dir}/pic.jpg").should be_false
-      Closync::Sync.new(config).push!
+    end
+    its 'missing files do not exist locally' do
+      File.exists?("#{local_dir}/missing.html").should be_false
+    end
+    its 'missing files exist on remote' do
+      File.exists?("#{remote_dir}/missing.html").should be_true
+    end
+    its 'stale files are a different size on remote' do
+      File.size("#{remote_dir}/stale.html").should_not == File.size("#{local_dir}/stale.html")
+    end
+  end
+
+  context 'after sync' do
+    before(:each) { Closync::Sync.new(config).push! }
+    its 'new files exist locally' do
+      File.exists?("#{local_dir}/index.html").should be_true
+      File.exists?("#{local_dir}/pic.jpg").should be_true
+    end
+    its 'new files exist on remote' do
       File.exists?("#{remote_dir}/index.html").should be_true
       File.exists?("#{remote_dir}/pic.jpg").should be_true
+    end
+    its 'missing files do not exist locally' do
+      File.exists?("#{local_dir}/missing.html").should be_false
+    end
+    its 'missing files do not exist on remote' do
+      File.exists?("#{remote_dir}/missing.html").should be_false
+    end
+    its 'stale files are the same size on remote' do
+      File.size("#{remote_dir}/stale.html").should == File.size("#{local_dir}/stale.html")
     end
   end
 
