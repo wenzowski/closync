537aaf94248bbb8337bd7949596bdc130f8e9352
Alexander Wenzowski <alexander@wenzowski.com>
2012-11-01 16:49:39 -0400

    try again?

--- spec/travis/env_spec.rb
+++ spec/travis/env_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 # https://github.com/travis-ci/travis-build/pull/36
+# check if joshk implemented...
 describe "Travis CI Environment Variables" do
   it { ENV['TRAVIS_BRANCH'].should == "feature/travis-branch" }
 end
